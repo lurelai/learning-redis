f174c90ddbdaa2438f02b3a7bc05345384c28236
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-04-04 21:32:01 -0300

    add a named network to compose

--- src/database/initRedis.js
+++ src/database/initRedis.js
@@ -1,6 +1,7 @@
 const { createClient } = require('redis')
 
-const client = createClient()
+const client = createClient({
+})
 
 const createConnection = async ()=>{
 	await client.on('error', err=>{
